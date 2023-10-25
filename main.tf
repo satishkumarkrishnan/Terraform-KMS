f7f80791a9d519af08b69d382184d496c1dfaff2
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-10-25 14:15:24 +0530

    updated .gitignore

--- main.tf
+++ main.tf
@@ -11,15 +11,14 @@ terraform {
 #To create KMS resource 
 resource "aws_kms_key" "tokyo_kms_key" {
   description             = "KMS key for Tokyo"
-  deletion_window_in_days = 1
+  deletion_window_in_days = 7
   key_usage               = "ENCRYPT_DECRYPT"
   enable_key_rotation     = true
 }
 
 #To create KMS Policy 
-
 resource "aws_kms_key_policy" "tokyo_kms_key_policy" {
-  key_id = aws_kms_key.tokyo_kms_key
+  key_id = aws_kms_key.tokyo_kms_key.arn
   policy = jsonencode({
     Id = "KMS policy"
     Statement = [
