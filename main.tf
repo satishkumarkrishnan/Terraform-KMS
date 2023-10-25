8f8929d4a898a22723c37b5363b8d2be4c0be8db
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-10-25 12:25:41 +0530

    tf files for aws KMS

--- main.tf
+++ main.tf
@@ -8,3 +8,32 @@ terraform {
     }
   }
 }
+#To create KMS resource 
+resource "aws_kms_key" "tokyo_kms_key" {
+  description             = "KMS key for Tokyo"
+  deletion_window_in_days = 1
+  key_usage               = "ENCRYPT_DECRYPT"
+  enable_key_rotation     = true
+}
+
+#To create KMS Policy 
+
+resource "aws_kms_key_policy" "tokyo_kms_key_policy" {
+  key_id = aws_kms_key.tokyo_kms_key
+  policy = jsonencode({
+    Id = "KMS policy"
+    Statement = [
+      {
+        Action = "kms:*"
+        Effect = "Allow"
+        Principal = {
+          AWS = "*"
+        }
+
+        Resource = "*"
+        Sid      = "Enable IAM User Permissions"
+      },
+    ]
+    Version = "2012-10-17"
+  })
+}
