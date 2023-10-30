e3ac87c3a55c58992513e83269f4d70e50b0a41c
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-10-30 19:17:14 +0530

    Adding KMS policy for CW log

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ resource "aws_kms_key_policy" "tokyo_kms_key_policy" {
             "Resource": "*",
             "Condition": {
                 "ArnLike": {
-                    "kms:EncryptionContext:aws:logs:arn": "arn:aws:logs:ap-northeast-1:297200899628:*"
+                    "kms:EncryptionContext:aws:logs:arn": "arn:aws:logs:ap-northeast-1:515149434592:log-group:tokyo_cw:*"                    
                 }
             }
         }    
