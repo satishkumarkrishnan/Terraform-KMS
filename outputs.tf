644c2125c585e3d10cd99bc5879efb184de9a495
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-06-12 15:33:40 +0530

    Added the KMS resource

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,6 @@ output "kms_arn" {
   value = aws_kms_key.ddsl_kms_key.arn
 }
 
-/*output "s3_bucket_name" {
-  value = aws_s3_bucket.kms_encrypted.bucket
-}/*
+# output "s3_bucket_name" {
+#  value = aws_s3_bucket.kms_encrypted.bucket
+#}
