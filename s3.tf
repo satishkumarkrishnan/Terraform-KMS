6e0708118270668422e729065bff29286adf6ccd
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-06-12 15:24:58 +0530

    Added the KMS resource

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 # S3 bucket to store App
-resource "aws_s3_bucket" "kms_encrypted" {
+/*resource "aws_s3_bucket" "kms_encrypted" {
   bucket = "tokyo-tf-kms"
   # Prevent accidental deletion of this S3 bucket
   lifecycle {
@@ -21,7 +21,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
 
   rule {
     apply_server_side_encryption_by_default {
-      kms_master_key_id = aws_kms_key.tokyo_kms_key.arn
+      kms_master_key_id = aws_kms_key.ddsl_kms_key.arn
       sse_algorithm = "aws:kms"
     }
   }
@@ -43,4 +43,4 @@ resource "aws_s3_object" "object" {
     prevent_destroy = false
   }
   depends_on = [aws_s3_bucket.kms_encrypted]
-}
+}*/
